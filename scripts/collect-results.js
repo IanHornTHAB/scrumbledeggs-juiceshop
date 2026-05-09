bf96386c8e0ea5c180fbf7b736c8d6bdaae0b623
Selina Riedel <anonym@example.invalid>
2026-05-09 15:21:09 +0000

    Update file collect-results.js

--- scripts/collect-results.js
+++ scripts/collect-results.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const REPORTS_DIR = 'app/artifacts/reports';
+const REPORTS_DIR = process.env.REPORTS_DIR || 'reports';
 const FILENAME = process.env.OUTPUT_FILENAME || 'scan-results-local.json';
 const OUTPUT_FILE = path.join(REPORTS_DIR, FILENAME);
 const PIPELINE_ID = process.env.CI_PIPELINE_ID || 'local-run';
